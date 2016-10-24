de88d2f4318f42f6683cc8aa25ec82f093be21cf
X3msnake <X3msnake@users.noreply.github.com>
2016-10-24 04:15:12 +0100

    Fixing wrong fork files :(
    The file that ares working unfortunately do not have a source name in them 
    the ino is based on kuwatay https://github.com/kuwatay/Arduino/blob/master/Segment_LCD_HT1621/Segment_LCD_HT1621.ino
    the lib i don't recall the origin :\

---
+++ ht1621.cpp
@@ -0,0 +1,139 @@
+#include <arduino.h>
+
+#include "HT1621.h"
+
+#define ADDR_MAX 128
+
+#define TAKE_CS()    digitalWrite(cs_pin, LOW)
+#define RELEASE_CS() digitalWrite(cs_pin, HIGH)
+
+inline void HT1621::initControlBus()
+{
+    pinMode(data_pin, OUTPUT);
+    pinMode(wr_pin, OUTPUT);
+    pinMode(rd_pin, OUTPUT);
+    pinMode(cs_pin, OUTPUT);
+
+    digitalWrite(cs_pin, HIGH);
+    digitalWrite(wr_pin, HIGH);
+    digitalWrite(rd_pin, HIGH);
+    digitalWrite(data_pin, HIGH);
+}
+
+inline bool HT1621::testMem()
+{
+    uint8_t test = 10;
+    writeMem(0x5a, test);
+    //if (readMem(0x5a) != test)
+    //    return false;
+    return true;
+}
+
+bool HT1621::begin()
+{
+    initControlBus();
+    //delay(100);   //uncomment if init fails
+    sendCommand(SYS_DIS);
+    if (! testMem())
+        return false;
+
+    memset(0, 0, ADDR_MAX);
+    sendCommand(SYS_EN);
+    sendCommand(LCD_ON);
+    return true;
+}
+
+void HT1621::writeBits(uint8_t data, uint8_t cnt)
+{
+    uint8_t bitmask;
+    while (cnt) {
+        digitalWrite(wr_pin, LOW);
+        uint8_t bitval = (data & (1 << (cnt - 1))) ? HIGH : LOW;
+        digitalWrite(data_pin, bitval);
+        digitalWrite(wr_pin, HIGH);
+        cnt--;
+    }
+}
+
+uint8_t HT1621::readBits(uint8_t cnt)
+{
+    uint8_t data = 0;
+    pinMode(data_pin, INPUT);
+    while (cnt) {
+        digitalWrite(rd_pin, LOW);
+        data += digitalRead(data_pin) << (cnt - 1);
+        digitalWrite(rd_pin, HIGH);
+        cnt--;
+    }
+    pinMode(data_pin, OUTPUT);
+    return data;
+}
+
+#define COMMAND_MODE 0b100
+void HT1621::sendCommand(uint8_t cmd, bool first /*= true*/, bool last /*= true*/)
+{
+    if (first) {
+        TAKE_CS();
+        writeBits(COMMAND_MODE, 3);
+    }
+    writeBits(cmd, 8);
+    writeBits(0, 1); //Last bit - don't care
+    if (last)
+        RELEASE_CS();
+}
+
+#define WRITE_MODE 0b101
+void HT1621::writeMem(uint8_t address, uint8_t data)
+{
+    TAKE_CS();
+    writeBits(WRITE_MODE, 3);
+    writeBits(address, 6);
+    writeBits(data, 4);
+    RELEASE_CS();
+}
+
+void HT1621::memset(uint8_t address, uint8_t data, uint8_t cnt)
+{
+  TAKE_CS();
+  writeBits(WRITE_MODE, 3);
+  writeBits(address, 6);
+  for (uint8_t i = 0; i < cnt; i++)
+    writeBits(data, 4);
+  RELEASE_CS();
+}
+
+//Write up to 256 values starting from address
+//Note: Data is 8-bit aligned. This is not vary efficient
+void HT1621::write(uint8_t address, uint8_t *data, uint8_t cnt)
+{
+    TAKE_CS();
+    writeBits(WRITE_MODE, 3);
+    writeBits(address, 6);
+    for (uint8_t i = 0; i < cnt; i++)
+        writeBits(data[i], 4);
+    RELEASE_CS();
+}
+
+#define READ_MODE 0b110
+uint8_t HT1621::readMem(uint8_t address)
+{
+    uint8_t data;
+    TAKE_CS();
+    writeBits(READ_MODE, 3);
+    writeBits(address, 6);
+    data = readBits(4);
+    RELEASE_CS();
+    return data;
+}
+
+#define READ_MODE 0b110
+void HT1621::read(uint8_t address, uint8_t *data, uint8_t cnt)
+{
+    TAKE_CS();
+    writeBits(READ_MODE, 3);
+    writeBits(address, 6);
+    for (uint8_t i = 0; i < cnt; i++)
+        data[i] = readBits(4);
+    RELEASE_CS();
+}
+
---
+++ ht1621.h
@@ -0,0 +1,63 @@
+/* HT1621 - Holtek RAM Mapping 32x4 LCD Controller */
+#ifndef HT1621_H
+#define HT1621_H
+
+#include <inttypes.h>
+
+class HT1621 {
+public:
+    enum {
+        SYS_DIS   = 0b00000000,
+        SYS_EN    = 0b00000001,
+        LCD_OFF   = 0b00000010,
+        LCD_ON    = 0b00000011,
+        TIMER_DIS = 0b00000100,
+        WDT_DIS   = 0b00000101,
+        TIMER_EN  = 0b00000110,
+        WDT_EN    = 0b00000111,
+        TONE_OFF  = 0b00001000,
+        TONE_ON   = 0b00001001,
+        
+        //Set bias to 1/2 or 1/3 cycle
+        //Set to 2,3 or 4 connected COM lines
+        BIAS_HALF_2_COM  = 0b00100000,
+        BIAS_HALF_3_COM  = 0b00100100,
+        BIAS_HALF_4_COM  = 0b00101000,
+        BIAS_THIRD_2_COM = 0b00100001,
+        BIAS_THIRD_3_COM = 0b00100101,
+        BIAS_THIRD_4_COM = 0b00101001,
+        
+        //Don't use
+        TEST_ON   = 0b11100000,
+        TEST_OFF  = 0b11100011
+    } Commands;
+
+    HT1621(uint8_t data, uint8_t wr, uint8_t rd, uint8_t cs) : 
+        data_pin(data), wr_pin(wr), rd_pin(rd), cs_pin(cs) {}
+
+    bool begin();
+
+    void sendCommand(uint8_t cmd, bool first = true, bool last = true);
+
+    void write(uint8_t address, uint8_t *data, uint8_t cnt);
+    void read(uint8_t address, uint8_t *data, uint8_t cnt);
+
+    void writeMem(uint8_t address, uint8_t data);
+    uint8_t readMem(uint8_t address);
+
+    void memset(uint8_t address, uint8_t data, uint8_t cnt);
+
+private:
+    void writeBits(uint8_t data, uint8_t cnt);
+    uint8_t readBits(uint8_t cnt);
+
+    inline void initControlBus();
+    inline bool testMem();
+    
+    uint8_t data_pin;
+    uint8_t wr_pin;
+    uint8_t rd_pin;
+    uint8_t cs_pin;
+};
+
+#endif //HT1621_H
